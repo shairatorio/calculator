19773baef75ecdf38961c23ecd5220a4697d98a9
shairatorio <shairatorio08@gmail.com>
2023-04-14 22:45:12 +0800

    Experiment functionality of addition and output screen display

--- script.js
+++ script.js
@@ -8,18 +8,22 @@ const equalsButton = document.querySelector('[data-equals]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
+let previousNumber = '';
+let currentNumber = '';
+let globalOperator = '';
+
 function audio(sound) {
     sound.play();
 }
 
 // add(a, b) - Returns the sum of two numbers a and b.
 function add(x,y) {
-    return x + y;
+  return x + y;
 }
 
 // // subtract(a, b) - Returns the difference between two numbers a and b.
 function subtract(x,y) {
-  return x + y;
+  return x - y;
 }
 
 // multiply(a, b) - Returns the product of two numbers a and b.
@@ -39,7 +43,10 @@ function percent(x,y) {
 
 // clear() - Clears the current input and resets the calculator.
 function clear() {
-  currentOperandTextElement.textContent = '';
+  currentNumber = '';
+  previousNumber = '';
+  previousOperandTextElement.innerHTML = '';
+  currentOperandTextElement.innerHTML = '';
 }
 
 // equals() - Evaluates the current expression and displays the result.
@@ -57,9 +64,28 @@ function negative() {
 
 }
 
-// // displayOutput(num) - Displays the given number on the calculator screen.
+// displayOutput(num) - Displays the given number on the calculator screen.
 function displayOutput(num) {
   currentOperandTextElement.append(num);
+}
+
+function pressedOperator(operator) {
+  //clear();
+  // console.log(currentNumber);
+
+  switch(operator) {
+    case '+': 
+      previousOperandTextElement.append(`${currentNumber}${operator}`);
+      previousNumber = currentNumber;
+      currentNumber = '';
+      globalOperator = operator;
+  }
+
+  console.log(`pressedOperator currentNumber = ${currentNumber}`);
+  console.log(`pressedOperator previousNumber = ${previousNumber}`);
+  // console.log(`pressedOperator currentOperand = ${currentOperandTextElement.textContent}`);
+  console.log(`pressedOperator previousOperand = ${previousOperandTextElement.textContent}`);
+  // console.log(`pressedOperator globalOperator = ${globalOperator}`);
 }
 
 function addGlobalEventListener(type, selector, callback) {
@@ -71,14 +97,41 @@ function addGlobalEventListener(type, selector, callback) {
 addGlobalEventListener("click", ".number", e => {  
   audio(click);
 
-  displayOutput(e.target.innerText);
-  console.log(e.target.innerText);
+  displayOutput(e.target.textContent);
+  currentNumber += e.target.textContent;
+
+  console.log(`addGlobalEventListener .number currentNumber = ${currentNumber}`);
+  console.log(`addGlobalEventListener previousNumber = ${previousNumber}`);
+  // console.log(`addGlobalEventListener .number currentOperand = ${currentOperandTextElement.textContent}`);
+  console.log(`addGlobalEventListener .number previousOperand = ${previousOperandTextElement.textContent}`);
+  // console.log(`addGlobalEventListener globalOperator = ${globalOperator}`);
+
+  if(globalOperator !== null && globalOperator !== '') {
+    // console.log('operator has value');
+    currentOperandTextElement.innerHTML = '';
+    displayOutput(e.target.textContent);
+    globalOperator = '';
+ }
+});
+
+addGlobalEventListener("click", ".operator", e => {  
+  audio(click);
+
+  // console.log(e.target.value);
+  pressedOperator(e.target.value);
 });
 
 addGlobalEventListener("click", ".clear", () => {  
   audio(click);
   clear();
 });
+
+addGlobalEventListener("click", "[data-equals]", () => {  
+  audio(click);
+  previousOperandTextElement.append(`${currentNumber}`);
+  currentOperandTextElement.textContent = add(parseInt(previousNumber),(parseInt(currentNumber)));
+});
+
 
 // btnList.forEach((button) => {
 //   button.addEventListener('click', e => {
